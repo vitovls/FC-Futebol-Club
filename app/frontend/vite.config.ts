a08ff958aee6b775f34d7e1418011c3ffc425d1c
Victor Veloso <vitovls37@gmail.com>
2026-05-09 19:12:40 -0300

    chore: fix vite.config.ts JSX support for .js and .jsx; add prop-types dependency

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -2,9 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{js,jsx}',
+    }),
+  ],
   server: {
     port: 3000,
     host: true,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.jsx': 'jsx',
+      },
+    },
   },
 });
